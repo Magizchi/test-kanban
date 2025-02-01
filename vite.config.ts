a55b6df64e5350bc4d1140fc8bf1d3a9df2d1613
Rajanan <g.rajan1993@gmail.com>
2025-02-01 09:38:53 +0100

    Config:(tsConfig&Vite):alias for absolute import

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@models': '/src/models',
+      '@constants': '/src/constants',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@routes': '/src/routes',
+      '@contexts': '/src/contexts',
+      '@template': '/src/template',
+      '@mocks': '/src/mocks',
+      '@utils': '/src/utils',
+      '@layouts': '/src/layouts',
+      '@hooks': '/src/hooks',
+    },
+  },
 });
